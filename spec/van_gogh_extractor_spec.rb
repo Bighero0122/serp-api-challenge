a58c68cdf4f1ef0edfb0b2269d1b21c0d3755781
kenneth-fox <147813840+Bighero0122@users.noreply.github.com>
2025-09-14 16:26:36 -0400

    test: add RSpec test suite

--- spec/van_gogh_extractor_spec.rb
+++ spec/van_gogh_extractor_spec.rb
@@ -1,14 +1,31 @@
 require_relative '../lib/van_gogh_extractor'
+require 'json'
 
 RSpec.describe VanGoghExtractor do
-  let(:html_content) { '<html><body>test</body></html>' }
+  let(:html_content) { File.read(File.join(__dir__, '..', 'files', 'van-gogh-paintings.html')) }
   let(:extractor) { VanGoghExtractor.new(html_content) }
+  let(:result) { extractor.extract_artworks }
 
-  describe '#extract_artworks' do
-    it 'returns hash with artworks array' do
-      result = extractor.extract_artworks
-      expect(result).to have_key(:artworks)
-      expect(result[:artworks]).to be_an(Array)
+  it 'extracts artworks array' do
+    expect(result[:artworks]).to be_an(Array)
+    expect(result[:artworks].length).to be >= 3
+  end
+
+  it 'includes required fields' do
+    result[:artworks].each do |artwork|
+      expect(artwork).to have_key(:name)
+      expect(artwork).to have_key(:extensions)
+      expect(artwork).to have_key(:link)
+      expect(artwork).to have_key(:image)
     end
+  end
+
+  it 'extracts Van Gogh paintings' do
+    names = result[:artworks].map { |a| a[:name].downcase }
+    expect(names.any? { |name| name.include?('starry') }).to be true
+  end
+
+  it 'generates valid JSON' do
+    expect { JSON.generate(result) }.not_to raise_error
   end
 end
