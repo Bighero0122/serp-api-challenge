57e6a6021df2fcedf43bfa60442ce55916443ba6
kenneth-fox <147813840+Bighero0122@users.noreply.github.com>
2025-09-14 15:29:26 -0400

    feat: implement JSON extraction from script tags

--- lib/van_gogh_extractor.rb
+++ lib/van_gogh_extractor.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'json'
+require 'uri'
 
 class VanGoghExtractor
   def initialize(html_content)
@@ -7,7 +8,36 @@ class VanGoghExtractor
   end
 
   def extract_artworks
-    # TODO: Implement extraction logic
-    { artworks: [] }
+    artworks = extract_from_json
+    { artworks: format_artworks(artworks) }
+  end
+
+  private
+
+  def extract_from_json
+    @doc.css('script').each do |script|
+      content = script.content
+      next unless content.include?('knowledge_graph')
+      
+      if match = content.match(/"artworks":\s*(\[.*?\])/m)
+        begin
+          return JSON.parse(match[1])
+        rescue JSON::ParserError
+          next
+        end
+      end
+    end
+    []
+  end
+
+  def format_artworks(raw_artworks)
+    raw_artworks.map do |artwork|
+      {
+        name: artwork['name'],
+        extensions: artwork['extensions'] || [],
+        link: artwork['link'],
+        image: artwork['image']
+      }
+    end
   end
 end
